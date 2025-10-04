054f5363ad4ba55acffa46b68899d78a9d826691
Nyrouse <rooty910@gmail.com>
2025-10-04 16:48:11 +0330

    done i guess?

---
+++ script/howToUse.js
@@ -0,0 +1,13 @@
+function setCookie(name, value, days) {
+  let expires = "";
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = `; expires=${date.toUTCString()}`;
+  }
+  document.cookie = `${name}=${value || ""}${expires}; path=/`;
+}
+
+document.querySelector("button").addEventListener("click", () => {
+  setCookie("readHowToUse", true, 30);
+});
--- script/script.js
+++ script/script.js
@@ -1,3 +1,30 @@
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
+  return null;
+}
+
+function setCookie(name, value, days) {
+  let expires = "";
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = `; expires=${date.toUTCString()}`;
+  }
+  document.cookie = `${name}=${value || ""}${expires}; path=/`;
+}
+
+window.addEventListener("load", () => {
+  const hasReadHowTo = getCookie("readHowToUse");
+
+  if (!hasReadHowTo) {
+    window.location.href = "./howToUse.html";
+  } else {
+    console.log("User has already read the How To Use page.");
+  }
+});
+
 const notePad = document.getElementById("notePad");
 const finishingRegex = /[ \.,!?;:]/;
 
