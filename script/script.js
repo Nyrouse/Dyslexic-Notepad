1c9aff021fa556c2cffc7342300d57c6af77f372
Nyrouse <rooty910@gmail.com>
2025-10-04 15:07:33 +0330

    making the word tracking better

--- script/script.js
+++ script/script.js
@@ -3,22 +3,43 @@ const notePad = $.getElementById("notePad");
 
 const finishingRegex = /[ .,?!]/;
 
-let lastTypedLetter;
+let lastTypedLetter = "";
 let lastWord = "";
+let scrambledWord = "";
+
+function scrambleLetters(word) {
+  if (word.length <= 3) return word;
+
+  const chars = word.split("");
+  const middle = chars.slice(1, -1);
+
+  for (let i = middle.length - 1; i > 0; i--) {
+    if (Math.random() < 0.3) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [middle[i], middle[j]] = [middle[j], middle[i]];
+    }
+  }
+
+  return chars[0] + middle.join("") + chars[chars.length - 1];
+}
 
 notePad.addEventListener("input", (event) => {
   lastTypedLetter = event.data;
 
   if (lastTypedLetter === null) {
-    lastWord = lastWord.slice(0, -1);
-  } else if (finishingRegex.test(lastTypedLetter)) {
+  } else if (finishingRegex.test(lastTypedLetter) || lastTypedLetter === " ") {
     if (lastWord.length > 0) {
-      console.log("Word is finished:", lastWord);
+      console.log("Word is finished:", scrambleLetters(lastWord));
       lastWord = "";
     }
   } else {
     lastWord += lastTypedLetter;
   }
 
-  console.log("Current lastWord:", lastWord);
+  if (lastWord.length > 0) {
+    scrambledWord = scrambleLetters(lastWord);
+  }
+
+  const text = notePad.value;
+  console.log(text.split(" "));
 });
