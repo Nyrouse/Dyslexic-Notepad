be2f882d970c605933fb6b8b1fc4b577ae522ae5
Nyrouse <rooty910@gmail.com>
2025-10-04 14:42:08 +0330

    tracking words

--- script/script.js
+++ script/script.js
@@ -1,9 +1,24 @@
 const $ = document;
 const notePad = $.getElementById("notePad");
 
-let lastTypedLatter;
+const finishingRegex = /[ .,?!]/;
+
+let lastTypedLetter;
+let lastWord = "";
 
 notePad.addEventListener("input", (event) => {
-  lastTypedLatter = event.data;
-  console.log(lastTypedLatter);
+  lastTypedLetter = event.data;
+
+  if (lastTypedLetter === null) {
+    lastWord = lastWord.slice(0, -1);
+  } else if (finishingRegex.test(lastTypedLetter)) {
+    if (lastWord.length > 0) {
+      console.log("Word is finished:", lastWord);
+      lastWord = "";
+    }
+  } else {
+    lastWord += lastTypedLetter;
+  }
+
+  console.log("Current lastWord:", lastWord);
 });
